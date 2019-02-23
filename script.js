823ce6f1596002d7fb770d87f402ab1534551165
Ogbara G. Godstar <colossalboss@users.noreply.github.com>
2019-02-23 17:58:09 +0100

    Add house info

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ let search = document.getElementById('search');
 let houses = document.getElementById('houses');
 let nameDiv = document.getElementById('name');
 
-let result;
 
 function init(path) {
     $.get(path, function(response) {
@@ -10,10 +9,26 @@ function init(path) {
         console.log(response.length);
 
         for (let i = 0; i < 6; i++) {
+
+            let div = document.createElement('div');
+            let name = document.createElement('h2')
+            let price = document.createElement('h3');
+            let loc = document.createElement('h3');
+
+            name.textContent = response[i].name;
+            price.textContent = response[i].price;
+            loc.textContent = response[i].location;
+
             let elem = document.createElement('img');
             elem.setAttribute('src', response[i].src);
             elem.classList.add('box');
-            houses.appendChild(elem);
+
+            div.appendChild(elem);
+            div.appendChild(name);
+            div.appendChild(price);
+            div.appendChild(loc);
+
+            houses.appendChild(div);
         }
 
     });
